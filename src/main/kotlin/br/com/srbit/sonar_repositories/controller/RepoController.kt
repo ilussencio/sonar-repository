d8696eb09b6e70a458a29bfcd2932130f5d18e0b
Lucas Ilussencio da Silva <ilussencio@mail.com>
2024-07-05 19:48:47 -0300

    update controller

--- src/main/kotlin/br/com/srbit/sonar_repositories/controller/RepoController.kt
+++ src/main/kotlin/br/com/srbit/sonar_repositories/controller/RepoController.kt
@@ -1,15 +1,20 @@
 package br.com.srbit.sonar_repositories.controller
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/")
-class RepoController {
+@Component
+class RepoController(
+    @Value("\${eureka.instance.instance-id}") var instance: String = ""
+){
     @GetMapping
     fun olaMundo():String {
-        print("REQUISIÇÃO FEITA COM SUCESSO!")
-        return "INICIALIZADO COM SUCESSO!"
+        println("REQUISIÇÃO FEITA COM SUCESSO!")
+        return instance
     }
 }
